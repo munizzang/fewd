bc2f50110e078ae100cb54f1c83ef8f57158916c
munizzang <mkim20@risd.edu>
2015-06-15 20:12:18 -0700

    new

---
+++ slider/js/slider.js
@@ -0,0 +1,17 @@
+// Define prototypical Slider function
+var numberOfStudents = 24;
+
+var init = function(){
+
+  var half = numberOfStudents / 2;
+  console.log(' half ',half);
+
+
+};
+
+init();
+
+console.log(' number of student ',numberOfStudents);
+
+
+/* end Slider */
