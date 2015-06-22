58dd549510d20287d61c15b4eee1e6b1db2b6c26
munizzang <mkim20@risd.edu>
2015-06-21 23:25:44 -0700

    new

--- slider 2/js/app.js
+++ slider 2/js/app.js
@@ -1,4 +1,2 @@
-// select #slider <div> and apply the Slider function
 var sliderElement = document.getElementById('slider');
-
 sliderElement.Slider();
--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,17 +1,65 @@
-// Define prototypical Slider function
-var numberOfStudents = 24;
+Element.prototype.Slider = function(){
 
-var init = function(){
+  var slider = this;
+  var wrapper = slider.children[0];
+  var slides = wrapper.children;
+  var position = 0;
+  var width = window.innerWidth;
+  var leftButton = document.createElement('div');
+  var rightButton = document.createElement('div');
 
-  var half = numberOfStudents / 2;
-  console.log(' half ',half);
+  this.createButtons = function() {
 
+    leftButton.classList.add('left');
+    rightButton.classList.add('right');
+    slider.appendChild(leftButton);
+    slider.appendChild(rightButton);
+
+    rightButton.addEventListener('mousedown', function(){
+
+      if(position > (width * (slides.length - 1)) * -1){
+        position = position - width;
+        wrapper.style.marginLeft = position + 'px';
+      }
+
+    });
+
+    leftButton.addEventListener('mousedown',function(){
+
+      if(position < 0) {
+        position = position + width;
+        wrapper.style.marginLeft = position + 'px';
+      }
+
+    });
+
+    };
+
+  this.resize = function() {
+
+    width = window.innerWidth;
+
+    wrapper.style.width = slides.length * width +'px';
+    wrapper.style.height = '100%';
+
+    for(var index=0; index<slides.length; index++) {
+      slides[index].style.width = width +'px';
+    }
+
+
+  };
+
+
+
+  this.init = function() {
+
+    this.createButtons();
+    this.resize();
+
+    window.addEventListener('resize',slider.resize);
+
+  };
+
+  this.init();
 
 };
-
-init();
-
-console.log(' number of student ',numberOfStudents);
-
-
-/* end Slider */
